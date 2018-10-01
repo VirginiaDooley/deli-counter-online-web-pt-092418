76cb2b41acf2ece3b872d4658f2a634821654e29
Virginia Dooley <vcdooley@gmail.com>
2018-10-01 19:54:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"] 
+another_deli =["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] 
+
+def line(katz_deli, other_deli, another_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  elsif 
+    puts "The line is currently: #{other_deli}."
+  else 
+    puts "The line is currently: #{another_deli}."
+end 
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+    puts katz_deli(1)
+  end
+end 
